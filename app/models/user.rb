39edc2e3738449b31bb9b8cdaa696db3d745d979
rysk1013 <rysk2908@gmail.com>
2021-02-14 16:23:10 +0900

    新規登録・ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
-
   validates :nickname, presence: true, uniqueness: true
   validates :last_name, presence: true
   validates :first_name, presence: true
