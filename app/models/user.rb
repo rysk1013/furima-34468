bb1637d71c5d3ff30be131f1438c4190872d6cf3
rysk1013 <rysk2908@gmail.com>
2021-02-14 17:00:04 +0900

    ユーザー本名・フリガナのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, uniqueness: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヶ一]+\z/} do
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+  
   validates :birthday, presence: true
 end
