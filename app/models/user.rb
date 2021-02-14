1712326e6d379dcf8ce6e8b02775772a1577fdbb
rysk1013 <rysk2908@gmail.com>
2021-02-15 01:04:58 +0900

    rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 
   validates :nickname, presence: true, uniqueness: true
 
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence: true, format: {with: /\A[ァ-ヶ一]+\z/} do
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/ } do
     validates :last_name_kana, presence: true
     validates :first_name_kana, presence: true
   end
-  
+
   validates :birthday, presence: true
 end
