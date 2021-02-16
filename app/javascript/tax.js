100d6d9ad705c54e611f550067756adcaaddd2e2
rysk1013 <rysk2908@gmail.com>
2021-02-16 11:44:33 +0900

    消費税の表示の実装

---
+++ app/javascript/tax.js
@@ -0,0 +1,14 @@
+function tax() {
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener('keyup', () => {
+    const value = itemPrice.value; 
+    const taxPrice = value / 10;
+    const addTaxContent = document.getElementById("add-tax-price");
+    if (value < 10) {
+      addTaxContent.innerHTML = 0;
+    } else {
+      addTaxContent.innerHTML = `${taxPrice}`;
+    }
+  });
+}
+window.addEventListener('load', tax);
