66c8f012ff5ad2fe01bae6549b8fec72048673b7
rysk1013 <rysk2908@gmail.com>
2021-02-16 15:54:28 +0900

    itemモデル単体テストコードの設定

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,113 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品出品' do
+    context '商品出品できるとき' do
+      it 'image,name,info,category_id,sales_status_id,shipping_id,prefecture_id,scheduled_delivery_id,price,user_idが存在すれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品できないとき' do
+      it 'nameが空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'nameが40文字以上では出品できない' do
+        @item.name = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+      end
+
+      it 'infoが空では出品できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+
+      it 'category_idが空では出品できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'category_idが1では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+
+      it 'sales_status_idが空では出品できない' do
+        @item.sales_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+
+      it 'sales_status_idが1では出品できない' do
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status Select")
+      end
+
+      it 'shipping_fee_idが空では出品できない' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+
+      it 'shipping_fee_idが1では出品できない' do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee Select")
+      end
+
+      it 'prefecture_idが空では出品できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'prefecture_idが1では出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture Select")
+      end
+
+      it 'scheduled_delivery_idが空では出品できない' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+
+      it 'scheduled_delivery_idが1では出品できない' do
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery Select")
+      end
+
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが全角数字では出品できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Half-width number")
+      end
+
+      it 'priceのが¥300〜¥9,999,999以外では出品できない' do
+        @item.price = '100000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is Out of settign range")
+      end
+    end
+  end
 end
