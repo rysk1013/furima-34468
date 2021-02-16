ac02229d0e1c7d054c35cfc38377db494db8816f
rysk1013 <rysk2908@gmail.com>
2021-02-16 14:42:43 +0900

    itemのFactoryBotの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {'アイテム'}
+    info {'商品説明'}
+    category_id {'2'}
+    sales_status_id {'2'}
+    shipping_fee_id {'2'}
+    prefecture_id {'2'}
+    scheduled_delivery_id {'2'}
+    price {'1000'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
