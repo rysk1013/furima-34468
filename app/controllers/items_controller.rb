660561de0e9fabae5ba1ba9d55ee8f8bc49a41e0
rysk1013 <rysk2908@gmail.com>
2021-02-17 21:54:29 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,8 +45,6 @@ class ItemsController < ApplicationController
 
   def access_restrictions
     item = Item.find(params[:id])
-    unless current_user.id == item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user.id == item.user_id
   end
 end
