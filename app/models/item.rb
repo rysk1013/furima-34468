4e5ef3263ccf394b87997c04b7239306bccbe7b5
rysk1013 <rysk2908@gmail.com>
2021-02-17 15:44:01 +0900

    rubocopの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+
   with_options presence: true do
     validates :image
     validates :name
@@ -11,26 +11,26 @@ class Item < ApplicationRecord
     validates :shipping_fee_id
     validates :prefecture_id
     validates :scheduled_delivery_id
-    validates :price, format: {with: /\A[0-9]\d+\z/, message: 'Half-width number'}
+    validates :price, format: { with: /\A[0-9]\d+\z/, message: 'Half-width number' }
   end
-  
-  validates :name, length: {maximum: 40}
-  
-  with_options numericality: {other_than: 1, message: 'Select'} do
+
+  validates :name, length: { maximum: 40 }
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
     validates :sales_status_id
     validates :shipping_fee_id
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
-  
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is Out of settign range'}
+
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is Out of settign range' }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee
-  belongs_to :prefecture 
+  belongs_to :prefecture
   belongs_to :scheduled_delivery
 end
-  
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name {'アイテム'}
-    info {'商品説明'}
-    category_id {2}
-    sales_status_id {2}
-    shipping_fee_id {2}
-    prefecture_id {2}
-    scheduled_delivery_id {2}
-    price {1000}
+    name { 'アイテム' }
+    info { '商品説明' }
+    category_id { 2 }
+    sales_status_id { 2 }
+    shipping_fee_id { 2 }
+    prefecture_id { 2 }
+    scheduled_delivery_id { 2 }
+    price { 1000 }
     association :user
 
     after(:build) do |item|
