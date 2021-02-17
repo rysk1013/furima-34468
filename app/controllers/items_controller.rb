fab9f905edf4af7fa3a22c95ff2bdd1fcc4afa8a
rysk1013 <rysk2908@gmail.com>
2021-02-17 21:36:11 +0900

    updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,13 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      @item = Item.find(params[:id])
+      render :edit
+    end
   end
 
   private
