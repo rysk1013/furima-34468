971fc2c4b651207f9005ae6dab3935c7e79e7391
rysk1013 <rysk2908@gmail.com>
2021-02-18 11:19:32 +0900

    set_itemメソッドの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :access_restrictions, only: [:edit, :update]
+  before_action :set_item, except: [:index, :new, :create]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -46,5 +44,9 @@ class ItemsController < ApplicationController
   def access_restrictions
     item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
