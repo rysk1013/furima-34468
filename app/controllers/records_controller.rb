0f186031bf6d912c163d2fb41a072a2860e9be5e
rysk1013 <rysk2908@gmail.com>
2021-02-19 01:11:08 +0900

    createのルーティング・コントローラー設定

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,4 +2,8 @@ class RecordsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :records, only: [:index]
+    resources :records, only: [:index, :create]
   end
 end
