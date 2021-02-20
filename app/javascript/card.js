31cc1d0ffb20567d45f199b1e19953d2facc1003
rysk1013 <rysk2908@gmail.com>
2021-02-20 17:42:54 +0900

    pay.jpのデータ送信の設定

---
+++ app/javascript/card.js
@@ -0,0 +1,35 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_89ba2f12593ac47bd3f9c357");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("record_place[number]"),
+      exp_month: formData.get("record_place[exp_month]"),
+      exp_year: `20${formData.get("record_place[exp_year]")}`,
+      cvc: formData.get("record_place[cvc]"),
+    };    
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const token0bj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", token0bj);
+      }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener('load', pay);
